2c463ca972b22d8e3d57fe2f6c9446c5b6fcd211
kasugasorata98 <jeromykho98@gmail.com>
2023-03-22 19:35:25 +0800

    only allowed authenticated users to access /list endpoints

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,9 @@
+import { validateCognitoJwtFields } from 'aws-jwt-verify/cognito-verifier'
 import express from 'express'
 const router = express.Router()
 import account from './account'
 import list from './list'
 
 router.use('/v1/account', account)
-router.use('/v1/list', list)
+router.use('/v1/list', validateCognitoJwtFields, list)
 export default router
