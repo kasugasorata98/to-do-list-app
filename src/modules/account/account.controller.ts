20e5c5882b3affb6dc6d5537029365c5ed1a5753
kasugasorata98 <jeromykho98@gmail.com>
2023-03-22 18:30:29 +0800

    added registerUser, getUser and refreshToken function into account controller

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -1,30 +1,31 @@
 import AccountService from './account.service'
 import jwtDecode from 'jwt-decode'
+import { User } from '../../database/model/user.model'
 class AccountController {
   private accountService: AccountService
   constructor() {
     this.accountService = new AccountService()
   }
-  //   async registerUser(idToken: string) {
-  //     const {
-  //       email,
-  //       sub,
-  //       name,
-  //       'cognito:username': username,
-  //     }: { [key: string]: string } = jwtDecode(idToken)
-  //     return this.userService.createUser({
-  //       email,
-  //       sub,
-  //       name,
-  //       username,
-  //     })
-  //   }
-  //   async getUser(userId: string): Promise<User | null> {
-  //     return this.userService.getUser(userId)
-  //   }
-  //   refreshToken(refreshToken: string) {
-  //     return this.userService.refreshToken(refreshToken)
-  //   }
+  async registerUser(idToken: string) {
+    const {
+      email,
+      sub,
+      name,
+      'cognito:username': username,
+    }: { [key: string]: string } = jwtDecode(idToken)
+    return this.accountService.createUser({
+      email,
+      sub,
+      name,
+      username,
+    })
+  }
+  async getUser(userId: string): Promise<User | null> {
+    return this.accountService.getUser(userId)
+  }
+  refreshToken(refreshToken: string) {
+    return this.accountService.refreshToken(refreshToken)
+  }
 }
 
 export default AccountController
--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -1,3 +1,45 @@
-class AccountService {}
+import {
+  CognitoIdentityProviderClient,
+  InitiateAuthCommand,
+} from '@aws-sdk/client-cognito-identity-provider'
+import userModel, { User } from '../../database/model/user.model'
+class AccountService {
+  private client: CognitoIdentityProviderClient
+  constructor() {
+    this.client = new CognitoIdentityProviderClient({
+      region: 'ap-southeast-1',
+    })
+  }
+  createUser({ name, sub, email, username }: { [key: string]: string }) {
+    return userModel.findOneAndUpdate(
+      {
+        email,
+      },
+      { name, sub, email, username },
+      { upsert: true }
+    )
+  }
+  getUser(userId: string): Promise<User | null> {
+    return userModel.findById(userId)
+  }
+  async refreshToken(refreshToken: string) {
+    const request = new InitiateAuthCommand({
+      AuthFlow: 'REFRESH_TOKEN_AUTH',
+      AuthParameters: {
+        REFRESH_TOKEN: refreshToken,
+      },
+      ClientId: process.env.COGNITO_CLIENT_ID as string,
+    })
+    const response = await this.client.send(request)
+
+    return {
+      access_token: response?.AuthenticationResult?.AccessToken,
+      refresh_token: response?.AuthenticationResult?.RefreshToken,
+      id_token: response?.AuthenticationResult?.IdToken,
+      token_type: response?.AuthenticationResult?.TokenType,
+      expires_in: response?.AuthenticationResult?.ExpiresIn,
+    }
+  }
+}
 
 export default AccountService
