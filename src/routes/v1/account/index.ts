941f9cfa435ac07caa9d3dbc01cf9da0bb9ea763
kasugasorata98 <jeromykho98@gmail.com>
2023-03-22 19:06:42 +0800

    added getAuthLink endpoint

--- src/routes/v1/account/index.ts
+++ src/routes/v1/account/index.ts
@@ -20,4 +20,10 @@ router.post('/authorize', async (req: Request, res: Response) => {
   }
 })
 
+router.get('/getAuthLink', (req: Request, res: Response) => {
+  res.json({
+    authorizeLink: cognitoController.getAuthorizationLink(),
+  })
+})
+
 export default router
