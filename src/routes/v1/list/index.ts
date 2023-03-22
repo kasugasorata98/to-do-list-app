5b1eb95f2802eea1eb748b4d04b44f07c9804c3c
kasugasorata98 <jeromykho98@gmail.com>
2023-03-22 19:10:17 +0800

    remove unwanted lib

--- src/routes/v1/list/index.ts
+++ src/routes/v1/list/index.ts
@@ -1,7 +1,4 @@
 import express, { Request, Response } from 'express'
-import { Token } from '../../../entities/token.entity'
-import AccountController from '../../../modules/account/account.controller'
-import CognitoController from '../../../modules/cognito/cognito.controller'
 
 const router = express.Router()
 
