1abd0846d5b6c803b75acdcb520b3abc7c9402cd
kasugasorata98 <jeromykho98@gmail.com>
2023-03-22 23:17:26 +0800

    update endpoint done

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,9 +3,11 @@ export const Constants = {
     REFRESH_TOKEN_AUTH: 'REFRESH_TOKEN_AUTH',
   },
   HTTP_CODES: {
+    OK: 200,
     CREATED: 201,
     BAD_REQUEST: 400,
     INTERNAL_SERVER_ERROR: 500,
+    NOT_MODIFIED: 304,
   },
   ERROR_MESSAGES: {
     TITLE_REQUIRED: "'title' is a required field",
--- src/routes/v1/list/index.ts
+++ src/routes/v1/list/index.ts
@@ -27,13 +27,58 @@ router.post('/', async (req: Request, res: Response) => {
 })
 
 router.get('/', async (req: Request, res: Response) => {
-  const { jwtPayload } = req.body
-  const { username } = jwtPayload
-  const response = await listController.getList({ username })
-  res.json(response)
+  try {
+    const { jwtPayload } = req.body
+    const { username } = jwtPayload
+    const response = await listController.getList({ username })
+    res.json(response)
+  } catch (err: any) {
+    return res.status(Constants.HTTP_CODES.INTERNAL_SERVER_ERROR).json({
+      message: err?.message,
+      code: err?.code,
+    })
+  }
 })
 
-router.patch('/', async (req: Request, res: Response) => {})
+router.patch('/', async (req: Request, res: Response) => {
+  try {
+    const { jwtPayload, title, description, isDone, toDoListId } = req.body
+    if (!title) {
+      return res.status(Constants.HTTP_CODES.BAD_REQUEST).json({
+        message: 'title is required',
+      })
+    }
+    if (!description) {
+      return res.status(Constants.HTTP_CODES.BAD_REQUEST).json({
+        message: 'description is required',
+      })
+    }
+    if (isDone === undefined || typeof isDone !== 'boolean') {
+      return res.status(Constants.HTTP_CODES.BAD_REQUEST).json({
+        message: 'isDone is required and must be boolean',
+      })
+    }
+    if (!toDoListId) {
+      return res.status(Constants.HTTP_CODES.BAD_REQUEST).json({
+        message: 'toDoListId is required',
+      })
+    }
+    const { username } = jwtPayload
+    const response = await listController.updateList({
+      username,
+      title,
+      description,
+      isDone,
+      toDoListId,
+    })
+    res.json(response)
+  } catch (err: any) {
+    return res.status(Constants.HTTP_CODES.INTERNAL_SERVER_ERROR).json({
+      message: err?.message,
+      code: err?.code,
+    })
+  }
+})
 
 router.delete('/', async (req: Request, res: Response) => {})
 
