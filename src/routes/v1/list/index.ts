32f06a7c556b834390746d805985b567c9d7bd2e
kasugasorata98 <jeromykho98@gmail.com>
2023-03-22 19:13:42 +0800

    clean up code

--- src/routes/v1/list/index.ts
+++ src/routes/v1/list/index.ts
@@ -2,12 +2,12 @@ import express, { Request, Response } from 'express'
 
 const router = express.Router()
 
-router.get('/', async (req: Request, res: Response) => {})
-
 router.post('/', async (req: Request, res: Response) => {})
 
-router.delete('/', async (req: Request, res: Response) => {})
+router.get('/', async (req: Request, res: Response) => {})
 
 router.patch('/', async (req: Request, res: Response) => {})
+
+router.delete('/', async (req: Request, res: Response) => {})
 
 export default router
