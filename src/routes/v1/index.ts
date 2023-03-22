1c548fd22424f50b007ecf21cb2d069869b66cec
kasugasorata98 <jeromykho98@gmail.com>
2023-03-22 22:02:48 +0800

    fix wrong import

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,9 @@
-import { validateCognitoJwtFields } from 'aws-jwt-verify/cognito-verifier'
 import express from 'express'
+import { validateJwt } from '../../middlewares/validateJwt.middleware'
 const router = express.Router()
 import account from './account'
 import list from './list'
 
 router.use('/v1/account', account)
-router.use('/v1/list', validateCognitoJwtFields, list)
+router.use('/v1/list', validateJwt, list)
 export default router
